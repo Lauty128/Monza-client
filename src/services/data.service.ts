6da9c0a4dc22c2844e84e01ce97653e538c53af3
Lauty128 <lautarosilverii@gmail.com>
2023-06-07 08:36:54 -0300

    function for storing response in local storage added

--- src/services/data.service.ts
+++ src/services/data.service.ts
@@ -2,14 +2,14 @@ import { marksInterface, marksResponseInterface,
         usersInterface, usersResponseInterface } from "@/models/services.model";
 
 //------- Utils
-//import { store_in_localStorage } from "@/utils/localStorage";
+import { store_in_localStorage } from "@/utils/localStorage";
 
 const api_domain = 'http://localhost:4000'
 
 export const get_users = async ():Promise<usersInterface[]>=>{
     const data:Promise<usersResponseInterface> = await fetch(`${api_domain}/api/users`).then(res => res.json())
 
-    //store_in_localStorage('users_list', (await data).data)
+    store_in_localStorage('users_list', (await data).data)
 
     return (await data).data
 }
@@ -17,7 +17,7 @@ export const get_users = async ():Promise<usersInterface[]>=>{
 export const get_marks = async ():Promise<marksInterface[]>=>{
     const data:Promise<marksResponseInterface> = await fetch(`${api_domain}/api/marks`).then(res => res.json())
 
-    //store_in_localStorage('marks_list', (await data).data)
+    store_in_localStorage('marks_list', (await data).data)
 
     return (await data).data
 }
